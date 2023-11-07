a0909e5e5ca96fa68244be2ebfdb8bd955319211
bruno <bruno@DESKTOP-S4N53FP>
2023-11-06 22:44:19 -0300

    versão definitiva da tela Veterinário

--- clinica/src/main/java/view/TelaCrudVeterinario.java
+++ clinica/src/main/java/view/TelaCrudVeterinario.java
@@ -21,12 +21,11 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class TelaCrudVeterinario extends JInternalFrame {
-	//atributos do projeto
+	// atributos do projeto
 	private Veterinario veterinario;
 	private VeterinarioControle controle = new VeterinarioControle();
-	
-	
-	//campos da tela
+
+	// campos da tela
 	private JTextField txtId;
 	private JTextField txtNome;
 	private JTextField txtEspecialidade;
@@ -35,6 +34,7 @@ public class TelaCrudVeterinario extends JInternalFrame {
 	private JButton btnInserir;
 	private JButton btnConsultar;
 	private JButton btnSalvar;
+
 	/**
 	 * Launch the application.
 	 */
@@ -60,68 +60,79 @@ public class TelaCrudVeterinario extends JInternalFrame {
 		setIconifiable(true);
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(null);
-		
+
 		JLabel lblTitulo = new JLabel("Veterin\u00E1rio");
 		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
 		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 16));
 		lblTitulo.setBounds(10, 11, 414, 14);
 		getContentPane().add(lblTitulo);
-		
+
 		JPanel painelCampos = new JPanel();
-		painelCampos.setBorder(new TitledBorder(null, "Dados do Veterin\u00E1rio", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		painelCampos.setBorder(new TitledBorder(null, "Dados do Veterin\u00E1rio", TitledBorder.LEADING,
+				TitledBorder.TOP, null, null));
 		painelCampos.setBounds(10, 36, 299, 194);
 		getContentPane().add(painelCampos);
 		painelCampos.setLayout(null);
-		
+
 		JLabel lblId = new JLabel("Id : ");
 		lblId.setBounds(10, 26, 46, 14);
 		painelCampos.add(lblId);
-		
+
 		txtId = new JTextField();
 		txtId.setEnabled(false);
 		txtId.setBounds(92, 23, 197, 20);
 		painelCampos.add(txtId);
 		txtId.setColumns(10);
-		
+
 		JLabel lblNome = new JLabel("Nome : ");
 		lblNome.setBounds(10, 60, 46, 14);
 		painelCampos.add(lblNome);
-		
+
 		txtNome = new JTextField();
 		txtNome.setEnabled(false);
 		txtNome.setBounds(92, 57, 197, 20);
 		painelCampos.add(txtNome);
 		txtNome.setColumns(10);
-		
+
 		txtEspecialidade = new JTextField();
 		txtEspecialidade.setEnabled(false);
 		txtEspecialidade.setColumns(10);
 		txtEspecialidade.setBounds(92, 95, 197, 20);
 		painelCampos.add(txtEspecialidade);
-		
+
 		JLabel lblEspecialidade = new JLabel("Especialidade : ");
 		lblEspecialidade.setBounds(10, 98, 90, 14);
 		painelCampos.add(lblEspecialidade);
-		
+
 		btnSalvar = new JButton("Salvar");
 		btnSalvar.setEnabled(false);
 		btnSalvar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (txtNome.getText().length() < 5) {
 					JOptionPane.showMessageDialog(btnSalvar, "O nome precisa ter no mínimo 5 caracteres!!!");
-				}else {
-					veterinario.setNome(txtNome.getText());
-					veterinario.setEspecialidade(txtEspecialidade.getText());
-					controle.inserir(veterinario);
-					JOptionPane.showMessageDialog(btnSalvar, "Veterinário cadastrado.");
-					txtId.setText(String.valueOf(veterinario.getIdVeterinario()));
+				} else {
+					//novo veterinario
+					if (veterinario == null) {
+						veterinario = new Veterinario();
+						veterinario.setNome(txtNome.getText());
+						veterinario.setEspecialidade(txtEspecialidade.getText());
+						controle.inserir(veterinario);
+						JOptionPane.showMessageDialog(btnSalvar, "Veterinário cadastrado.");
+						txtId.setText(String.valueOf(veterinario.getIdVeterinario()));
+					}else {
+						//alterar
+						veterinario.setNome(txtNome.getText());
+						veterinario.setEspecialidade(txtEspecialidade.getText());
+						controle.alterar(veterinario);
+						JOptionPane.showMessageDialog(btnSalvar, "Veterinário atualizado.");
+					}
 					definirEstadoConsulta();
 				}
 			}
 		});
 		btnSalvar.setBounds(200, 126, 89, 23);
 		painelCampos.add(btnSalvar);
-		
+
 		JButton btnCancelar = new JButton("Cancelar");
 		btnCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -130,33 +141,34 @@ public class TelaCrudVeterinario extends JInternalFrame {
 		});
 		btnCancelar.setBounds(102, 126, 89, 23);
 		painelCampos.add(btnCancelar);
-		
+
 		JPanel panel = new JPanel();
 		panel.setBounds(319, 36, 105, 194);
 		getContentPane().add(panel);
 		panel.setLayout(null);
-		
+
 		btnInserir = new JButton("Novo");
 		btnInserir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				definirEstadoEdicao();
 				limparCampos();
-				veterinario = new Veterinario();
 				txtNome.requestFocus();
 			}
 		});
 		btnInserir.setBounds(10, 13, 89, 23);
 		panel.add(btnInserir);
-		
+
 		btnAlterar = new JButton("Alterar");
 		btnAlterar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				definirEstadoEdicao();
+				txtNome.requestFocus();
 			}
 		});
 		btnAlterar.setEnabled(false);
 		btnAlterar.setBounds(10, 49, 89, 23);
 		panel.add(btnAlterar);
-		
+
 		btnExcluir = new JButton("Excluir");
 		btnExcluir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -170,29 +182,28 @@ public class TelaCrudVeterinario extends JInternalFrame {
 		btnExcluir.setEnabled(false);
 		btnExcluir.setBounds(10, 85, 89, 23);
 		panel.add(btnExcluir);
-		
+
 		btnConsultar = new JButton("Consultar");
 		btnConsultar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String id = JOptionPane.showInputDialog("Digite o id do usuário: ");
 				veterinario = controle.buscarPorId(Integer.parseInt(id));
-				if (veterinario!=null) {
+				if (veterinario != null) {
 					txtId.setText(String.valueOf(veterinario.getIdVeterinario()));
 					txtNome.setText(veterinario.getNome());
 					txtEspecialidade.setText(veterinario.getEspecialidade());
 					definirEstadoConsulta();
-				}
-				else {
+				} else {
 					JOptionPane.showMessageDialog(null, "Não existe veterinário com esse código");
 				}
 			}
 		});
 		btnConsultar.setBounds(10, 121, 89, 23);
 		panel.add(btnConsultar);
-		
+
 		JButton btnFechar = new JButton("Fechar");
 		btnFechar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {				
+			public void actionPerformed(ActionEvent e) {
 				dispose();
 			}
 		});
@@ -200,7 +211,7 @@ public class TelaCrudVeterinario extends JInternalFrame {
 		panel.add(btnFechar);
 
 	}
-	
+
 	private void definirEstadoInicial() {
 		limparCampos();
 		txtNome.setEnabled(false);
@@ -211,7 +222,7 @@ public class TelaCrudVeterinario extends JInternalFrame {
 		btnConsultar.setEnabled(true);
 		btnSalvar.setEnabled(false);
 	}
-	
+
 	private void definirEstadoEdicao() {
 		txtNome.setEnabled(true);
 		txtEspecialidade.setEnabled(true);
@@ -221,7 +232,7 @@ public class TelaCrudVeterinario extends JInternalFrame {
 		btnConsultar.setEnabled(false);
 		btnSalvar.setEnabled(true);
 	}
-	
+
 	private void definirEstadoConsulta() {
 		txtNome.setEnabled(false);
 		txtEspecialidade.setEnabled(false);
@@ -231,7 +242,7 @@ public class TelaCrudVeterinario extends JInternalFrame {
 		btnConsultar.setEnabled(true);
 		btnSalvar.setEnabled(false);
 	}
-	
+
 	private void limparCampos() {
 		txtId.setText("");
 		txtNome.setText("");
